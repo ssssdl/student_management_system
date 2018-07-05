8fa16b58ba98b494746a816403ddaae7135e6c54
ssssdl <1463620240@qq.com>
2018-07-05 12:02:43 +0800

    更新 sqlconfig.php

--- config/sqlconfig.php
+++ config/sqlconfig.php
@@ -1,7 +1,7 @@
 <?php
     define('SQL_HOST','localhost');//数据库地址
     define("SQL_USER","root");//数据库用户名
-    define("SQL_PASSWORD","admin@shan");//数据库密码
+    define("SQL_PASSWORD","");//数据库密码
     define("SQL_DATABASE","education");//连接的数据库名字
     define("SQL_PORT","3306");//数据库端口号,默认为3306
 ?>
